fda85d1a275685cb064fa5e8d9b615d5f17fb587
giggity-hub <imigstefan@gmail.com>
2021-09-18 16:50:10 +0200

    scurr

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,4 +1,15 @@
 # f = open("demo.txt", "a")
 # f.write("sheeeeesh\n")
+import os
+# print(os.listdir('Schulferien'))
 
-print("sheeeeeeeeeeeeeesh")
+
+directory = 'Schulferien'
+for year in os.listdir('Schulferien'):
+    year_directory = os.path.join(directory, year)
+
+    for bundesland in os.listdir(year_directory):
+        print(bundesland)
+    # print(os.path.join(directory, filename))
+
+    # https://stackoverflow.com/questions/57921401/push-to-origin-from-github-action/58393457#58393457
