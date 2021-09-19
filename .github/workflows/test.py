b04724758d16037e30552899d48e3862f996fee7
giggity-hub <imigstefan@gmail.com>
2021-09-19 11:35:01 +0200

    did sum stuff

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,15 +1,28 @@
 # f = open("demo.txt", "a")
 # f.write("sheeeeesh\n")
 import os
+import csv
+import json
 # print(os.listdir('Schulferien'))
 
+ferien_dict = {}
 
 directory = 'Schulferien'
 for year in os.listdir('Schulferien'):
     year_directory = os.path.join(directory, year)
+    ferien_dict[year] = {}
 
-    for bundesland in os.listdir(year_directory):
-        print(bundesland)
-    # print(os.path.join(directory, filename))
+    for bundesland_csv in os.listdir(year_directory):
+        bundesland_path = os.path.join(year_directory, bundesland_csv)
+        bundesland_code = bundesland_csv.split('.')[0]
 
-    # https://stackoverflow.com/questions/57921401/push-to-origin-from-github-action/58393457#58393457
+        with open(bundesland_path, encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, skipinitialspace=True)
+            
+            def addBundesland(row):
+                row['bundesland'] = bundesland_code
+                return row
+ 
+            ferien_dict[year][bundesland_code] = [addBundesland(row) for row in reader]
+
+print(json.dumps(ferien_dict, sort_keys=True, indent=4))
